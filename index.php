d0babca172bc0320359355b027e0d831989f0c2e
Samiah Ford <sdfordmy364.bellevue.edu>
2024-02-04 00:21:13 -0600

    sdf 02/04/2024 : updating the landing page

--- index.php
+++ index.php
@@ -4,13 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Moffat Bay Lodge</title>
 </head>
 
-<body>
+<body class="landing-page">
     <?php
     readfile("shared/navigation.html");
     ?>
+    <div id="left-text">
+        <h1>Moffat Bay Lodge</h1><br>
+        <p>Welcome to Moffat Bay Lodge, your idyllic retreat nestled in the heart of the breathtaking Pacific Northwest.
+            Embrace the serene beauty of nature as you embark on an unforgettable journey surrounded by towering evergreens
+            and the tranquil waters of Moffat Bay. Our lodge is a haven for those seeking a perfect blend of rustic charm
+            and modern comfort.</p><br>
+        <p>At Moffat Bay Lodge, we invite you to unwind in cozy cabins, each designed to provide a warm sanctuary after a day of
+            exploration. Immerse yourself in the splendor of the region, whether it's hiking through lush trails, kayaking on
+            the pristine bay, or simply enjoying the mesmerizing sunsets by the lodge's private waterfront.</p><br>
+        <p>Our dedicated staff is committed to ensuring your stay is seamless and memorable. Let Moffat Bay Lodge be your
+            escape into nature's embrace, where every moment becomes a cherished memory. Welcome to a haven where the Pacific
+            Northwest's beauty meets unparalleled hospitality.</p><br>
+        <button><a href="#">Book Now!</a></button>
+    </div>
 </body>
 
 </html>
